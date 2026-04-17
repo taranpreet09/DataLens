06f17097e7985eeafe0f08891caea2e3502dcde5
taranpreet09 <kaur.taran223@gmail.com>
2026-04-17 11:45:34 +0530

    commit

--- data_frontend/src/pages/Home.jsx
+++ data_frontend/src/pages/Home.jsx
@@ -6,7 +6,6 @@ export default function Home() {
 
 
 
-
   const fadeUpVariant = {
     hidden: { opacity: 0, y: 40 },
     visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeOut" } }
